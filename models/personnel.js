e40d5ed2f8a931452d7be44cb711e31ddb6885e9
Curti.s_ <matthewscurtis.dev81@gmail.com>
2019-07-05 23:49:50 +0300

    :wrench: Update personnel model

---
+++ models/personnel.js
@@ -0,0 +1,88 @@
+/* jshint indent: 1 */
+const bcrypt = require("bcryptjs");
+
+module.exports = function(sequelize, DataTypes) {
+  let Personnel = sequelize.define(
+    "personnel",
+    {
+      personnel_id: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      personnel_onames: {
+        type: DataTypes.STRING(45),
+        allowNull: true
+      },
+      personnel_fname: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+      },
+      personnel_email: {
+        type: DataTypes.STRING(45),
+        allowNull: true
+      },
+      personnel_phone: {
+        type: DataTypes.STRING(45),
+        allowNull: true
+      },
+      personnel_password: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+        defaultValue: "e10adc3949ba59abbe56e057f20f883e"
+      },
+      personnel_status: {
+        type: DataTypes.INTEGER(1),
+        allowNull: false,
+        defaultValue: "1"
+      },
+      last_login: {
+        type: DataTypes.DATE,
+        allowNull: true
+      },
+      personnel_type_id: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+        defaultValue: "1"
+      },
+      reset_password: {
+        type: DataTypes.INTEGER(1),
+        allowNull: false,
+        defaultValue: "1"
+      }
+    },
+    {
+      tablename: "personnel",
+      timestamps: false,
+
+      hooks: {
+        beforeCreate: function(personnel, options) {
+          try {
+            bcrypt.genSalt(10, (err, salt) => {
+              if (err) return err;
+              bcrypt.hash(
+                personnel.personnel_password,
+                salt,
+                null,
+                (err, hash) => {
+                  if (err) return err;
+                  personnel.personnel_password = hash;
+                  return options;
+                }
+              );
+            });
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      }
+    }
+  );
+
+  Personnel.prototype.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.personnel_password);
+  };
+
+  return Personnel;
+};
